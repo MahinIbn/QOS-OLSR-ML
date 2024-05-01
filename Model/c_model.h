1b541b8f0824132be56d463b53a8ab0283825089
Muhamed Nebuhan <111905864+mnebu@users.noreply.github.com>
2024-05-01 14:28:29 +0400

    Saved model and its C versions
    Model_cg_w10.h5 is the ML model used for the final tests. This version is identified as sdp_v3 across the QoS test results. Other files are the C files of model which where generated by Keras2C library.

---
+++ Model/c_model.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void c_model(k2c_tensor* conv1d_input_input, k2c_tensor* dense_3_output); 
+void c_model_initialize(); 
+void c_model_terminate(); 
